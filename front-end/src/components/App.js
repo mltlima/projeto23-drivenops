5426fe7988ad30ef32f4d77feeaa7acfdb2fb857
Ubuntu <ubuntu@ip-172-31-93-239.ec2.internal>
2022-08-19 19:15:18 +0000

    updates

--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -6,6 +6,7 @@ function App() {
     
   useEffect(async () => {
     const API_URL = process.env.REACT_APP_BASE_URL;
+   
     try {
       const response = await axios.get(`${API_URL}/students/random`);
       const student = response.data;
